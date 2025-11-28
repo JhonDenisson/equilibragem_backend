d885ce71e9c990798293ec773ee7bb4aa4641067
Jhonata Denisson <jhonata.denisson@inovaxio.com.br>
2025-11-28 16:11:56 -0300

    Adds libs to setup project

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+
+const app = new Elysia().use(swagger()).listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
